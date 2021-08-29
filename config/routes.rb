d2fd64b36d49b2162f9487f236b2fdc1f66f8ce9
visnusantos1203 <visnu.santos1203@gmail.com>
2021-08-29 11:30:19 +0800

    changed min height

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
   
   #got an error when I changed categories#something -> categories#something
   
-  # get '/categories' => 'categories#index' ## index
-  # get '/categories/new' => 'categories#new', as: 'categories_new' ##new
-  # get '/categories/:id' => 'categories#show', as: 'categories_show' ## show
-  # post '/categories' => 'categories#create', as: 'categories_create' #create
-  # get '/categories/edit/:id' => 'categories#edit', as: 'categories_edit' # edit
-  # post '/categories/:id' => 'categories#update', as: 'categories_update' # edit
   root 'home#index'
 
   resources :categories do
